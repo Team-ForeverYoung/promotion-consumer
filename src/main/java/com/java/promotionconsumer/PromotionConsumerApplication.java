8d807460ccc4ea2917824f3a68ab1b8693db78b1
woosungking <abuga991008@gmail.com>
2025-05-28 19:05:23 +0900

    Initial commit

---
+++ src/main/java/com/java/promotionconsumer/PromotionConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.java.promotionconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionConsumerApplication.class, args);
+	}
+
+}
