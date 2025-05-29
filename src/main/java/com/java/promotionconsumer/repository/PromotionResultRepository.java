1c8d285452283fac1b34fd0420137d3df8f77898
woosungking <abuga991008@gmail.com>
2025-05-29 20:36:04 +0900

    feat: Mongodb Repo

---
+++ src/main/java/com/java/promotionconsumer/repository/PromotionResultRepository.java
@@ -0,0 +1,9 @@
+package com.java.promotionconsumer.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.java.promotionconsumer.entity.PromotionResultLog;
+@Repository
+public interface PromotionResultRepository extends MongoRepository<PromotionResultLog, String> {
+}
