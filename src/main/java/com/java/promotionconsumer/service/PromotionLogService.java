2f6ea67b8a1f704b876d7545d7829a9fdb525727
woosungking <abuga991008@gmail.com>
2025-05-29 20:36:30 +0900

    feat:

---
+++ src/main/java/com/java/promotionconsumer/service/PromotionLogService.java
@@ -0,0 +1,22 @@
+package com.java.promotionconsumer.service;
+
+import org.springframework.stereotype.Service;
+
+import com.java.promotionconsumer.dto.PromotionResultMessage;
+import com.java.promotionconsumer.entity.PromotionResultLog;
+import com.java.promotionconsumer.factory.PromotionResultLogFactory;
+import com.java.promotionconsumer.repository.PromotionResultRepository;
+
+@Service
+public class PromotionLogService {
+	private final PromotionResultRepository promotionResultRepository;
+
+	public PromotionLogService(PromotionResultRepository promotionResultRepository) {
+		this.promotionResultRepository = promotionResultRepository;
+	}
+
+	public void savePromotionResultLog(PromotionResultMessage message){
+		promotionResultRepository.save(PromotionResultLogFactory.createPromotionResultLog(message));
+	}
+
+}
