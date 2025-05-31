f91acc70c79ab4f08ce22e73ea6e4e3335071d25
woosungking <abuga991008@gmail.com>
2025-05-31 13:58:04 +0900

    chore:

--- src/main/java/com/java/promotionconsumer/dto/PromotionResultMessage.java
+++ src/main/java/com/java/promotionconsumer/dto/PromotionResultMessage.java
@@ -1,6 +1,7 @@
 package com.java.promotionconsumer.dto;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 public class PromotionResultMessage {
@@ -9,6 +10,9 @@ public class PromotionResultMessage {
 	private Long eventId;
 	private String userName;
 	private String eventName;
+
+	public PromotionResultMessage() {
+	}
 
 	public PromotionResultMessage(boolean success, Long userId, Long eventId, String userName, String eventName) {
 		this.success = success;
