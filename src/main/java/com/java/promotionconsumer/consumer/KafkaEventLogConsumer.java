a24c4b3020b49a8cce9e8c1f21ff16a48644770a
woosungking <abuga991008@gmail.com>
2025-05-31 14:56:10 +0900

    refactor: debug logging

--- src/main/java/com/java/promotionconsumer/consumer/KafkaEventLogConsumer.java
+++ src/main/java/com/java/promotionconsumer/consumer/KafkaEventLogConsumer.java
@@ -1,5 +1,7 @@
 package com.java.promotionconsumer.consumer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -7,7 +9,7 @@ import com.java.promotionconsumer.dto.EventResultMessage;
 import com.java.promotionconsumer.service.EventLogService;
 @Component
 public class KafkaEventLogConsumer implements EventLogConsumer {
-
+	private static final Logger log = LoggerFactory.getLogger(KafkaEventLogConsumer.class);
 	private final static String TOPIC = "promotion_result";
 	private final EventLogService eventLogService;
 
@@ -18,6 +20,8 @@ public class KafkaEventLogConsumer implements EventLogConsumer {
 	@Override
 	@KafkaListener(topics = TOPIC, containerFactory = "promotionResultConsumerFactoryContainer")
 	public void promotionResultConsumer(EventResultMessage message) {
+		log.info("메시지 수신완료");
+		log.info("메시지 수신완료"+message.getEventName() + message.getUserName());
 		eventLogService.savePromotionResultLog(message);
 	}
 
--- src/main/java/com/java/promotionconsumer/service/EventLogService.java
+++ src/main/java/com/java/promotionconsumer/service/EventLogService.java
@@ -1,5 +1,7 @@
 package com.java.promotionconsumer.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.java.promotionconsumer.dto.EventResultMessage;
@@ -9,12 +11,15 @@ import com.java.promotionconsumer.repository.EventResultRepository;
 @Service
 public class EventLogService {
 	private final EventResultRepository eventResultRepository;
-
+	private final static Logger log = LoggerFactory.getLogger(EventLogService.class);
 	public EventLogService(EventResultRepository eventResultRepository) {
 		this.eventResultRepository = eventResultRepository;
 	}
 
 	public void savePromotionResultLog(EventResultMessage message){
+		log.info("로그 저장 중 ......");
+		log.info("로그 저장 중 ......");
+		log.info("로그 저장 중 ......");
 		eventResultRepository.save(EventResultLogFactory.createPromotionResultLog(message));
 	}
 
