353f0fea154329610a531351ac45f71256ac3c62
woosungking <abuga991008@gmail.com>
2025-06-10 22:48:28 +0900

    refactor: 리스너 매개변수 문자열로 변경
    CDC적용으로 인한 변경

--- src/main/java/com/java/promotionconsumer/consumer/EventLogConsumer.java
+++ src/main/java/com/java/promotionconsumer/consumer/EventLogConsumer.java
@@ -3,5 +3,5 @@ package com.java.promotionconsumer.consumer;
 import com.java.promotionconsumer.dto.EventResultMessage;
 
 public interface EventLogConsumer {
-	void promotionResultConsumer(EventResultMessage message);
+	void promotionResultConsumer(String message);
 }
